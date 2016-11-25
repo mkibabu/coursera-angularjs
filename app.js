5a03480754591eebfcc37fc0312f8c6dc6ba2336
Michael Muraya <mmuraya@lulu.com>
2016-11-25 00:32:26 -0500

    Week 1 lesson 6: Implementing NameCalculator example in Angular

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-// ng exposes only the 'angular' object globally
 (function () {
   'use strict';
-  // define the module, with the name of the application and an array of dependencies.
-  angular.module('myFirstApp', [])
-  // define the viewmodel (controller) of the view (the html), and the function
-  // that describes how it works. Function takes $scope, which is an ng construct
-  // that allows us to share data between viewmodel and view
-  .controller('MyFirstController', function($scope) {
-    // $scope can have properties on it that are exposed to the view
-    $scope.name = 'Michael';
-    $scope.sayHello = function() {
-      return 'Hello Coursera!'
+
+  angular.module('NameCalculator', [])
+  .controller('NameCalculatorController', function($scope) {
+    $scope.nameInput = "";
+    $scope.nameNumericValue = 0;
+
+    $scope.displayNumeric = function () {
+      $scope.nameNumericValue = calculateNumericForString($scope.nameInput);
+    }
+
+    function calculateNumericForString(string) {
+      var totalStringValue = 0;
+      for (var i = 0; i < string.length; i++) {
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue;
     }
   })
 
