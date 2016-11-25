700c3462ede6e91f9863f116f2c22ab47b60894b
Michael Muraya <mmuraya@lulu.com>
2016-11-25 02:57:19 -0500

    Week 1 lesson 10: Protecting dependency injection from minification

--- di.js
+++ di.js
@@ -2,7 +2,22 @@
   'use strict';
 
   angular.module('DIApp', [])
+  // minified JS retains string literals, but minifies function and variable
+  // names. As such, $scope, $filter and $injector are minified, which makes
+  // angular not work (it won't know that whatever single-xcter name $scope is
+  // given, for instance, is a placeholder for (and actually means) $scope).
+  // String literals aren't minified, however. So we can change the controller
+  // call below to take an array of string literals (the arguments needed - in
+  // correct order) and the controller function.
+
+  // original, that would be ruined by minification
+  // .controller('DIController', diController);
+  // new
+  // .controller('DIController', ['$scope', '$filter', diController])
+  // Even better, and more readable; directly inject the controller's angular
+  // arguments, i.e:
   .controller('DIController', diController);
+  diController.$inject = ['$scope', '$filter']
 
   function diController($scope, $filter, $injector) {
     $scope.name = 'Michael';
@@ -11,8 +26,6 @@
       var upCase = $filter('uppercase');
       $scope.name = upCase($scope.name)
     };
-
-    console.log($injector.annotate(diController));
 
   }
 
