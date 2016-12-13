ac5b5f244d91fb53939d0f008a5a0840aa2e89e5
Michael Muraya <mmuraya@lulu.com>
2016-12-13 06:05:34 -0500

    Week 2 lesson 14: Digest cycle part III

--- week2/digestCycle.js
+++ week2/digestCycle.js
@@ -6,6 +6,9 @@
 
   counterController.$inject = ['$scope'];
   function counterController($scope) {
+    // we use ng-model to bind this variable and set up a watch
+    $scope.name = 'Michael';
+
     // this value will only be incremented once. This is to test that its watcher
     // function is only invoked once.
     $scope.onceCounter = 0;
@@ -26,18 +29,29 @@
     $scope.incrementIncrementingCounter  = function() {
       $scope.incrementingCounter++;
     }
+
     // one way to set up a watcher is by using the watch function. It takes two
     // arguments; the value to watch, and a function that responds to that value
     // changing. The responding function gets the old and new values of the variable
-    // it's watching from the Angular context
-    $scope.$watch('onceCounter', function(newValue, oldValue){
-      console.log('old onceCounter value: ', oldValue);
-      console.log('new onceCounter value: ', newValue)
-    });
+    // it's watching from the Angular context.
+    // NOTE: This is not the recommended way to set up watches! DO NOT USE $WATCH IN
+    // YOUR CONTROLLER!!
+    // $scope.$watch('onceCounter', function(newValue, oldValue){
+    //   console.log('old onceCounter value: ', oldValue);
+    //   console.log('new onceCounter value: ', newValue)
+    // });
+    //
+    // $scope.$watch('incrementingCounter', function(newValue, oldValue){
+    //   console.log('old incrementingCounter value: ', oldValue);
+    //   console.log('new incrementingCounter value: ', newValue)
+    // });
 
-    $scope.$watch('incrementingCounter', function(newValue, oldValue){
-      console.log('old incrementingCounter value: ', oldValue);
-      console.log('new incrementingCounter value: ', newValue)
+    // use this to peek into the digest loop's execution. The digest function will
+    // go through all the watchers and execute the functions therein.
+    $scope.$watch(function() {
+        // Each time a varaible changes, this should get logged twice (the digest
+        // cycle involves two loops, usually). If nothing changes, it'll only run nce.
+        console.log('Digest loop fired!');
     });
   }
 
